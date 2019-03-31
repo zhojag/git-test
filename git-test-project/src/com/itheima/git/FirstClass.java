1829a370435e4594585e88f0974d0f2ef20a093f
zho <zho@zho.cn>
2019-03-31 15:57:15 +0800

    天章道:   修改FirstClass

--- git-test-project/src/com/itheima/git/FirstClass.java
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -14,12 +14,14 @@ public class FirstClass {
         System.out.println("hello git project");
         System.out.println("hello git project");
         System.out.println("hello git project");
+        //天昊: hello test
 
     }
 
     public void test(){
         int i = 0;
         i+= 10;
+        System.out.println(i);
     }
 
 }
